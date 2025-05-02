4c30be4cba7c197e1232c91c6b96caa8a2727b1b
Matt Iadanza <matthew.iadanza@stfc.ac.uk>
2025-05-02 12:02:01 +0100

    Add particles annotations

--- tomobabel/models/annotation.py
+++ tomobabel/models/annotation.py
@@ -20,6 +20,7 @@ def check_input_dims(inputs: List[object]):
 
 class AnnotationType(str, Enum):
     point = "point"
+    particle = "particle_coodinate"
     sphere = "sphere"
     cuboid = "cuboid"
     vector = "vector"
@@ -34,69 +35,70 @@ class AnnotationType(str, Enum):
     text = "text"
 
 
-class Point(CoordsLogical):
-    type: str = AnnotationType.point
-
-
-class Vector(Annotation):
-    """A defined by two points"""
-
-    type: str = AnnotationType.vector
-    start: Point
-    end: Point
-
-    @model_validator(mode="after")
-    def input_dims_match(self) -> Vector:
-        check_input_dims([self.start, self.end])
-        return self
-
-    @property
-    def points(self) -> np.ndarray:
-        """
-        Get the start and end points of the vector as arrays
-
-        Returns:
-            np.ndarry: the start and end point arrays
-        """
-        start_coords = self.start.coord_array
-        end_coords = self.end.coord_array
-        return np.array([start_coords, end_coords])
-
-    @property
-    def vector(self) -> np.ndarray:
-        """
-        Get the vector as a 3x1 array
-
-        Returns:
-            np.ndarray: the vector array
-        """
-        p1, p2 = self.points
-        return p2 - p1
-
-    @property
-    def unit_vector(self) -> np.ndarray:
-        """
-        Get the unit vector as a 3x1 array
-
-        Returns:
-            np.ndarray: the unit vector array
-        """
-        norm = np.linalg.norm(self.vector)
-        return self.vector / norm
-
-
-class Sphere(Annotation):
-    """A sphere or circle of radius 'radius' centered on a coordinate"""
-
-    type: str = AnnotationType.sphere
-    center: Point = Field(
-        default=..., description="The center point of the circle/sphere"
+class Cone(Annotation):
+    type: str = AnnotationType.cone
+    vector: Vector = Field(
+        default=..., description="Vector for the center line of the cylinder"
     )
-    radius: float = Field(default=..., description="The sphere diameter")
+    start_diameter: float = Field(
+        default=..., description="Diameter of the base of the cone"
+    )
+    end_diameter: float = Field(
+        default=0.0,
+        description=(
+            "Diameter of the end of the cone, if 0.0, the cone comes to a sharp point,"
+            "otherwise it is cut off"
+        ),
+    )
 
-    @property
-    def coord_array(self):
-        return self.center.coord_array
+
+class Cuboid(Annotation):
+    """A cuboidal box defined by two or three vectors"""
+
+    type: str = AnnotationType.cuboid
+    center: Point = Field(default=..., description="The center of the box")
+    v1: Vector = Field(
+        default=...,
+        description="A vector that points to a face of the box from the center point",
+    )
+    v2: Optional[Vector] = Field(
+        default=None,
+        description=(
+            "A vector that points to the 2nd face of the box, if None it will be set "
+            "as 90° from v1 to make a square/cube"
+        ),
+    )
+    v3: Optional[Vector] = Field(
+        default=None,
+        description=(
+            "A vector that points to the 3nd face of the box, if None it will be set as"
+            "90° from v2 to make a square/cube"
+        ),
+    )
+
+    # TODO: write function that calculates V2 and V3 automatically for a cube
+
+
+class Cylinder(Annotation):
+    type: str = AnnotationType.cylinder
+    vector: Vector = Field(
+        default=..., description="Vector for the center line of the cylinder"
+    )
+    diameter: float = Field(default=..., description="The diameter of the cylinder")
+
+
+class FitMap(Annotation):
+    """Annotation for a fitted map"""
+
+    type: str = AnnotationType.map
+    center: Point = Field(default=..., description="Coords of the center of the map")
+    file: str = Field(default=..., description="Path to the map file")
+    transformation: AffineTransform = Field(
+        default=AffineTransform(),
+        description=(
+            "Tranformation applied to fit the map. IE: Affine transform for rotation"
+        ),
+    )
 
 
 class Ovoid(Annotation):
@@ -147,66 +149,14 @@ class Ovoid(Annotation):
             raise ValueError("Ovoid waist point is not on central vector")
 
 
-class Cuboid(Vector):
-    """A cuboidal box defined by two or three vectors"""
-
-    type: str = AnnotationType.cuboid
-    center: Point = Field(default=..., description="The center of the box")
-    v1: Vector = Field(
-        default=...,
-        description="A vector that points to a face of the box from the center point",
-    )
-    v2: Optional[Vector] = Field(
-        default=None,
-        description=(
-            "A vector that points to the 2nd face of the box, if None it will be set "
-            "as 90° from v1 to make a square/cube"
-        ),
-    )
-    v3: Optional[Vector] = Field(
-        default=None,
-        description=(
-            "A vector that points to the 3nd face of the box, if None it will be set as"
-            "90° from v2 to make a square/cube"
-        ),
-    )
-
-    # TODO: write function that calculates V2 and V3 automatically for a cube
+class Point(CoordsLogical):
+    type: str = AnnotationType.point
 
 
-class Spline(Annotation):
-    type: str = AnnotationType.spline
-    points: List[Point] = Field(default=..., description="At least two points")
-
-    @field_validator("points")
-    def at_least_three_points(cls, value: List[Point]) -> List[Point]:
-        if len(value) < 3:
-            raise ValueError("A spline must have at least 3 points")
-        return value
-
-
-class Cylinder(Annotation):
-    type: str = AnnotationType.cylinder
-    vector: Vector = Field(
-        default=..., description="Vector for the center line of the cylinder"
-    )
-    diameter: float = Field(default=..., description="The diameter of the cylinder")
-
-
-class Cone(Annotation):
-    type: str = AnnotationType.cone
-    vector: Vector = Field(
-        default=..., description="Vector for the center line of the cylinder"
-    )
-    start_diameter: float = Field(
-        default=..., description="Diameter of the base of the cone"
-    )
-    end_diameter: float = Field(
-        default=0.0,
-        description=(
-            "Diameter of the end of the cone, if 0.0, the cone comes to a sharp point,"
-            "otherwise it is cut off"
-        ),
+class Particle(Point):
+    type: str = AnnotationType.particle
+    fom: Optional[float] = Field(
+        default=None, description="Figure of merit for autopicking"
     )
 
 
@@ -223,26 +173,100 @@ class Shell(Annotation):
     )
 
 
-class FitMap(Annotation):
-    """Annotation for a fitted map"""
+class Sphere(Annotation):
+    """A sphere or circle of radius 'radius' centered on a coordinate"""
 
-    type: str = AnnotationType.map
-    center: Point = Field(default=..., description="Coords of the center of the map")
-    file: str = Field(default=..., description="Path to the map file")
-    transformation: AffineTransform = Field(
-        default=AffineTransform(),
-        description=(
-            "Tranformation applied to fit the map. IE: Affine transform for rotation"
-        ),
+    type: str = AnnotationType.sphere
+    center: Point = Field(
+        default=..., description="The center point of the circle/sphere"
     )
+    radius: float = Field(default=..., description="The sphere diameter")
+
+    @property
+    def coord_array(self):
+        return self.center.coord_array
+
+
+class Spline(Annotation):
+    type: str = AnnotationType.spline
+    points: List[Point] = Field(default=..., description="At least two points")
+
+    @field_validator("points")
+    def at_least_three_points(cls, value: List[Point]) -> List[Point]:
+        if len(value) < 3:
+            raise ValueError("A spline must have at least 3 points")
+        return value
+
+
+class Vector(Annotation):
+    """A defined by two points"""
+
+    type: str = AnnotationType.vector
+    start: Point
+    end: Point
+
+    @model_validator(mode="after")
+    def input_dims_match(self) -> Vector:
+        check_input_dims([self.start, self.end])
+        return self
+
+    @property
+    def points(self) -> np.ndarray:
+        """
+        Get the start and end points of the vector as arrays
+
+        Returns:
+            np.ndarry: the start and end point arrays
+        """
+        start_coords = self.start.coord_array
+        end_coords = self.end.coord_array
+        return np.array([start_coords, end_coords])
+
+    @property
+    def vector(self) -> np.ndarray:
+        """
+        Get the vector as a 3x1 array
+
+        Returns:
+            np.ndarray: the vector array
+        """
+        p1, p2 = self.points
+        return p2 - p1
+
+    @property
+    def unit_vector(self) -> np.ndarray:
+        """
+        Get the unit vector as a 3x1 array
+
+        Returns:
+            np.ndarray: the unit vector array
+        """
+        norm = np.linalg.norm(self.vector)
+        return self.vector / norm
+
+
+class AnnotationSetTypes(str, Enum):
+    """
+    Types for sets of annotations
+    """
+
+    general = "annotations"
+    particle_coords = "particle_coordinates"
 
 
 class AnnotationSet(ConfiguredBaseModel):
-    name: Optional[str] = Field(
-        default="Annotations",
+    name: str = Field(
+        default=AnnotationSetTypes.general,
         description="The name of the annotation set EG 'Membranes' or 'Ribosomes'",
     )
     annotations: List[Annotation] = Field(default=..., description="The annotations")
+
+
+class ParticleCoordinatesSet(AnnotationSet):
+    type: str = AnnotationSetTypes.particle_coords
+    particles: List[Particle] = Field(
+        default_factory=[], description="Picked particle coorindates"
+    )
 
 
 # TODO: Add Surface and Volume Annotation types.  Investigate the best way to do this
@@ -257,6 +281,8 @@ Cuboid.model_rebuild()
 Cylinder.model_rebuild()
 FitMap.model_rebuild()
 Ovoid.model_rebuild()
+Particle.model_rebuild()
+ParticleCoordinatesSet.model_rebuild()
 Point.model_rebuild()
 Shell.model_rebuild()
 Sphere.model_rebuild()
---
+++ tomobabel/models/annotation_factory.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from tomobabel.models.annotation import Point
+
+
+def point(coords: np.ndarray) -> Point:
+    if len(coords) == 2:
+        point = Point(x=coords[0, 0], y=coords[1, 0])
+    elif len(coords) == 3:
+        point = Point(x=coords[0, 0], y=coords[1, 0], z=coords[2, 0])
+    else:
+        raise ValueError("coords must be 2x1 or 3x1 array")
+    return point
